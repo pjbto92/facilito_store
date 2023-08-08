709d6e4aea99d9a762d9f35188f68402f81c8633
pjbto92 <pjbto92@gmail.com>
2023-08-08 13:50:34 -0400

    Ahora

--- facilito_store/facilito_store/urls.py
+++ facilito_store/facilito_store/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    
+    path("", views.index,name='index'),
     path('admin/', admin.site.urls),
 ]
--- facilito_store/facilito_store/views.py
+++ facilito_store/facilito_store/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index (request):
-        return HttpResponse('Hola Mundo')
+        return HttpResponse('Hola Mundo desde el archivo views.py')
