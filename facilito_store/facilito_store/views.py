e6ae6f518756e8a0e9b1ff4a60e623b40cd90699
pjbto92 <pjbto92@gmail.com>
2023-08-08 14:09:37 -0400

    se agrega el gitignore

--- facilito_store/facilito_store/views.py
+++ facilito_store/facilito_store/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index (request):
-        return HttpResponse('Hola Mundo desde el archivo views.py')
+        return render (request,'index.html',{
+            
+        })
