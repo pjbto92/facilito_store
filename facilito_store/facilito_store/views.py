7a2f455d182d9c9e95f1bf188fc5c54896fd46ac
pjbto92 <pjbto92@gmail.com>
2023-08-08 15:42:32 -0400

    Vistas con Bootstrap

--- facilito_store/facilito_store/views.py
+++ facilito_store/facilito_store/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 
 def index (request):
         return render (request,'index.html',{
-            'message': 'Nuevo Mensaje'
-        })S
+            'message': 'Listado de Productos',
+            'title': 'Productos',
+            'products': [
+                
+                {'title':'Playera', 'price': 5, 'stock': True},
+                {'title':'Camisa', 'price': 7, 'stock': True},
+                {'title':'Mochila', 'price': 20, 'stock': False},
+                
+                
+             ]
+        })
