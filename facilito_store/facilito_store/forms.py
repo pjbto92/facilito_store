2f9cd8c0f92ff492cc3df6162794f969ae5878ad
pjbto92 <pjbto92@gmail.com>
2023-08-11 16:08:32 -0400

    Ultimo del Viernes

--- facilito_store/facilito_store/forms.py
+++ facilito_store/facilito_store/forms.py
@@ -1,5 +1,28 @@
 from django import forms
+
+
 class RegisterForm(forms.Form):
- username = forms.CharField(required=True, min_length=4, max_length=50,widget=forms.TextInput(attrs={'class':'form-control'}))
- email = forms.EmailField(required=True)
- password= forms.CharField(required=True)
+    username = forms.CharField(
+        required=True,
+        min_length=4,
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "id": "username", "placeholder": "username"}
+        ),
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "id": "email",
+                "placeholder": "example@juacalo.com",
+            }
+        ),
+    )
+
+
+    password = forms.CharField(
+    required=True,
+    widget=forms.PasswordInput(attrs={"class": "form-control", "id": "password"}),
+)
