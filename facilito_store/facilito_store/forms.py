6dec616ce818057a027dce61b066e7632992e8a8
pjbto92 <pjbto92@gmail.com>
2023-08-17 17:21:20 -0400

    Nuevo

--- facilito_store/facilito_store/forms.py
+++ facilito_store/facilito_store/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 
+from django.contrib.auth.models import  User
+
 class RegisterForm(forms.Form):
     username = forms.CharField(
         required=True,
@@ -10,6 +12,8 @@ class RegisterForm(forms.Form):
             attrs={"class": "form-control", "id": "username", "placeholder": "username"}
         ),
     )
+    
+
     email = forms.EmailField(
         required=True,
         widget=forms.EmailInput(
@@ -21,8 +25,25 @@ class RegisterForm(forms.Form):
         ),
     )
 
-
     password = forms.CharField(
-    required=True,
-    widget=forms.PasswordInput(attrs={"class": "form-control", "id": "password"}),
-)
+        required=True,
+        widget=forms.PasswordInput(attrs={"class": "form-control", "id": "password"}),
+    )
+    password2 = forms.CharField(label='confirmar password',
+                                required=True,
+                                widget=forms.PasswordInput(attrs={
+                                    'class': 'form-control'
+                                    })) 
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('El username ya se encuentra en uso nde mykure')
+        return username
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('El Email tambien ya se encuentra en uso nde mykure')
+        return email
+
--- facilito_store/facilito_store/views.py
+++ facilito_store/facilito_store/views.py
@@ -2,50 +2,70 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
-from django.contrib import messages 
+from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from .forms import RegisterForm
 from django.contrib.auth import authenticate
 
-def index (request):
-        return render (request,'index.html',{
-            'message': 'Listado de Productos',
-            'title': 'Productos',
-            'products': [
-                
-                {'title':'Playera', 'price': 5, 'stock': True},
-                {'title':'Camisa', 'price': 7, 'stock': True},
-                {'title':'Mochila', 'price': 20, 'stock': False},
-                 {'title':'Laptop', 'price': 120, 'stock': True},
-                
-                
-             ]
-        })
-        
-        
-        
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username') #diccionario
-        password = request.POST.get('password') 
-         
-        user = authenticate( username=username,password=password)
-        if user:
-            login(request,user)
-            messages.success(request,'Bienvenido {}'.format(user.username))
-            return redirect('index')
-        else: 
-            messages.error(request,'Usuario y contraseña no validos')            
+from django.contrib.auth.models import  User
 
-    return render(request,'users/login.html', {})   
+ 
+
+def index(request):
+    return render(
+        request,
+        "index.html",
+        {
+            "message": "Listado de Productos",
+            "title": "Productos",
+            "products": [
+                {"title": "Playera", "price": 5, "stock": True},
+                {"title": "Camisa", "price": 7, "stock": True},
+                {"title": "Mochila", "price": 20, "stock": False},
+                {"title": "Laptop", "price": 120, "stock": True},
+            ],
+        },
+    )
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")  # diccionario
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, "Bienvenido {}".format(user.username))
+            return redirect("index")
+        else:
+            messages.error(request, "Usuario y contraseña no validos")
+
+    return render(request, "users/login.html", {})
+
 
 def logout_view(request):
-    logout (request,)
-    messages.success(request, 'Sesion cerrada corectamente')
-    return redirect('login')
+    logout(
+        request,
+    )
+    messages.success(request, "Sesion cerrada corectamente")
+    return redirect("login")
+
 
 def register(request):
-    form = RegisterForm()
-    return render(request,'users/register.html',{'form' : form})
+    form = RegisterForm(request.POST or None
+    )
     
+    if request.method== 'POST' and form.is_valid():
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+
+          
+        user = User.objects.create_user(username,email,password)
+        if user:
+            login(request, user)
+            messages.success(request,'Usuario Creado Exitosamente')
+            return redirect('index')
+    return render(request, "users/register.html", {"form": form})
